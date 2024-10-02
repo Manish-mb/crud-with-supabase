05d8d5aa02edcca98382f2afbc4db9b38dff30dd
Manish-mb <manishmb2797@gmail.com>
2024-10-02 12:22:04 +0530

    post request and index file add bootstrap CDN link

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,48 @@ import { supabase } from './createClient';
 function App() {
   const [user, setuser] = useState([]);
 
+  const [adduser, setAdduser] = useState({
+    firstname: "",
+    lastname: ""
+  })
+
+
+  const Adduserdata = async (event) => {
+    event.preventDefault();
+
+    const { data, error } = await
+      supabase.from('user')
+        .insert([{
+          firstname: adduser.firstname, lastname: adduser.lastname
+        }]);
+    if (error) {
+      console.error('Error adding user:', error.message);
+    } else {
+      console.log('User added successfully:', data);
+     
+      getuser();
+
+      // Clear the form after submission
+      setAdduser({
+        firstname: "",
+        lastname: ""
+      });
+    }
+  }
+
+const Deleteuser = async () =>{
+
+}
+
+  const handleChange = (event) => {
+
+    setAdduser(prevFormData => ({
+
+      ...prevFormData,
+      [event.target.name]: event.target.value
+
+    }));
+  }
 
 
   async function getuser() {
@@ -19,30 +61,53 @@ function App() {
 
   return (
     <>
-      <div className='d-flex justify-content-center'>
-        <div className="container mt-3">
-          <h2>Striped Rows</h2>
-          <table class="table table-striped">
-            <thead>
-              <tr>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>Email</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>John</td>
-                <td>Doe</td>
-                <td>john@example.com</td>
-                <td> <button>Edit</button></td>
-                <td>  <button>Delete</button></td>
 
-              </tr>
-
-            </tbody>
-          </table>
+      <div className='row'>
+        <div className='col-lg-6 '>
+          <div className="container p-3">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>Firstname</th>
+                  <th>Lastname</th>
+                  <th className='text-center'>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {user.map((users) =>
+                  <tr key={users.id}>
+                    <td>{users.firstname}</td>
+                    <td>{users.lastname}</td>
+                    <td><button className='btn btn-primary btn-sm'>Edit</button></td>
+                    <td><button className='btn btn-danger btn-sm' onClick={() =>{Deleteuser(user.id)}} >Delete</button></td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className='col-lg-6'>
+          <div className="container p-5">
+            <form onSubmit={Adduserdata}>
+              <div className="form-group">
+                <label >firstaname</label>
+                <input type="text" name="firstname"
+                  value={adduser.firstname}
+                  onChange={handleChange}
+                  className="form-control"
+                  placeholder=" " />
+              </div>
+              <div className="form-group">
+                <label >lastname</label>
+                <input type="text" name="lastname"
+                  value={adduser.lastname}
+                  onChange={handleChange}
+                  className="form-control"
+                  id="" placeholder="" />
+              </div>
+              <button type="submit" className="btn btn-primary  mt-3" >Submit</button>
+            </form>
+          </div>
         </div>
       </div>
     </>
